a56f74b39c14d0fe86ef4b296a3c7f9a3115e106
Job SAUL <job.saul2@gmail.com>
2018-01-12 10:00:10 +0100

    Affichage des Themes de formation MVC - 11-01-2017

---
+++ GestionCentreFormation_IHM/Controllers/ThemeFormationController.cs
@@ -0,0 +1,21 @@
+﻿using GestionCentreFormation_Metier;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GestionCentreFormation_IHM.Controllers
+{
+    public class ThemeFormationController : Controller
+    {
+        // GET: ThemeFormation
+        public ActionResult Index()
+        {
+            FormationEntities oContexteDonnees = new FormationEntities(); 
+            return View(new FormationEntities().Theme);
+        }
+
+        
+    }
+}
